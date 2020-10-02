abbd91ab51afd97a77f62a8a59b207686bfcdf7b
raphaeljp <rjp.raphael@gmail.com>
2020-10-02 11:29:55 -0300

    ADDING CRUD AND TESTS

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,27 +1,49 @@
-import { Body, Controller, Delete, Get, HttpCode, HttpStatus, Param, Patch, Post, Query } from '@nestjs/common';
+import {
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Inject,
+    Param,
+    Patch,
+    Post,
+    Query
+} from '@nestjs/common';
 import { CoffeesService } from "./coffees.service";
 import { CreateCoffeeDto } from "./dto/create-coffee.dto";
 import { UpdateCoffeeDto } from "./dto/update-coffee.dto";
+import {PaginationQueryDto} from "../common/dto/pagination-query.dto";
+import {REQUEST} from "@nestjs/core";
+import {Request} from "express";
+import {Public} from "../common/decorators/public.decorator";
+import {ParseIntPipe} from "../common/pipes/parse-int.pipe";
+import {Protocol} from "../common/decorators/protocol.decorator";
+import {ApiTags} from "@nestjs/swagger";
 
+@ApiTags('coffees')
 @Controller('coffees')
 export class CoffeesController {
-    constructor(private readonly coffeesService: CoffeesService) {}
+    constructor(
+        private readonly coffeesService: CoffeesService,
+        @Inject(REQUEST) private readonly request: Request
+    ) {}
 
+    @Public()
     @Get()
-    findAll(@Query() paginationQuery) {
-        // const { limit, offset } = paginationQuery;
-        return this.coffeesService.findAll();
+    findAll(
+        @Protocol('https') protocol: string,
+        @Query() paginationQuery: PaginationQueryDto
+    ) {
+        return this.coffeesService.findAll(paginationQuery);
     }
 
     @Get(':id')
-    findOne(@Param('id') id: number) {
-        console.log(typeof id)
+    findOne(@Param('id', ParseIntPipe) id: number) {
         return this.coffeesService.findOne('' + id);
     }
 
     @Post()
     create(@Body() createCoffeeDto: CreateCoffeeDto) {
-        console.log(createCoffeeDto instanceof CreateCoffeeDto);
         return this.coffeesService.create(createCoffeeDto);
     }
 
--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,9 +1,19 @@
-import { Module } from '@nestjs/common';
-import { CoffeesController } from "./coffees.controller";
-import { CoffeesService } from "./coffees.service";
+import {Module} from '@nestjs/common';
+import {CoffeesController} from "./coffees.controller";
+import {CoffeesService} from "./coffees.service";
+import {TypeOrmModule} from "@nestjs/typeorm";
+import {Coffee} from "./entities/coffee.entity";
+import {Flavor} from "./entities/flavor.entity";
+import {Event} from "../events/entities/event.entity";
+import {ConfigModule} from "@nestjs/config";
+import coffeesConfig from './config/coffees.config';
 
 @Module({
+    imports: [
+        TypeOrmModule.forFeature([Coffee, Flavor, Event]),
+        ConfigModule.forFeature(coffeesConfig)],
     controllers: [CoffeesController],
-    providers: [CoffeesService]
+    providers: [CoffeesService],
+    exports: [CoffeesService]
 })
 export class CoffeesModule {}
